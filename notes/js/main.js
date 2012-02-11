f9548d555bb37aae955ce5f9e8972a55a7d3bc27
Tanner Smith <tanner@tssoftware.net>
2012-02-11 15:24:46 -0500

    Make deleting select an element around it.

--- notes/js/main.js
+++ notes/js/main.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
-	$("#outline li").click(selectRow);
+	$("#outline li").click(function() {
+		selectRow($(this));
+	});
+
 	$("#outline li .btn-delete").click(deleteRow);
 })
 
-function selectRow() {
-	$(this).children(".btn-group").removeClass("hidden");
-	$(this).siblings().children(".btn-group").addClass("hidden");
+function selectRow(row) {
+	row.children(".btn-group").removeClass("hidden");
+	row.siblings().children(".btn-group").addClass("hidden");
 }
 
 function deleteRow() {
+	var next = $(this).parents("li").next();
+
+	if (next.length >= 1) {
+		selectRow(next.eq(0));
+	} else {
+		var previous = $(this).parents("li").prev();
+
+		if (previous.length >= 1) {
+			selectRow(previous.eq(0));
+		}
+	}
+
 	$(this).parents("li").remove()
 }
